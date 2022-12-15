b94e5c152284ebfc368974631c85274d0590a4ab
anup kumar <kanup414@gmail.com>
2022-12-15 17:53:39 +0530

    done

---
+++ block-BNaacj/sample/test/article.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var arcticleSchema = new Schema ({
+    title : String,
+    count : Number,
+    favorities: [String],
+    marks: [Number],
+    passwrd: {type: String, minlength: 5, maxlength: 15}
+}, {timestamps: true})
